3d57cc7bfe99e9591bbc3108ad96bdde40601191
pedrodrim <pedrohenriquedrim@gmail.com>
2024-05-20 21:51:15 -0300

    Adicionando documentacao no repositorio typescript

--- src/leaflet.timedimension.layer.js
+++ src/leaflet.timedimension.layer.js
@@ -1,4 +1,9 @@
-import { LatLngBounds, Layer, Evented, Util } from "leaflet";
+import {
+    LatLngBounds,
+    Layer,
+    Evented,
+    Util
+} from "leaflet";
 import * as timeDimensionModule from './leaflet.timedimension';
 
 /*
@@ -9,118 +14,180 @@ import * as timeDimensionModule from './leaflet.timedimension';
  */
 
 export const TimeDimensionLayer = Layer.extend({
-  includes: Evented.prototype,
-  options: {
-    opacity: 1,
-    zIndex: 1,
-  },
+    includes: Evented.prototype,
+    options: {
+        opacity: 1,
+        zIndex: 1,
+    },
 
-  initialize: function (layer, options) {
-    Util.setOptions(this, options || {});
-    this._map = null;
-    this._baseLayer = layer;
-    this._currentLayer = null;
-    this._timeDimension = this.options.timeDimension || null;
-  },
+    /**
+     * Timedimension "Layer" initializer
+     * @param {Layer} layer default leaflet layer
+     * @param {object} options layer options 
+     * @returns timedimension layer
+     */
+    initialize: function(layer, options) {
+        Util.setOptions(this, options || {});
+        this._map = null;
+        this._baseLayer = layer;
+        this._currentLayer = null;
+        this._timeDimension = this.options.timeDimension || null;
+    },
 
-  addTo: function (map) {
-    map.addLayer(this);
-    return this;
-  },
+    /**
+     * Observable like method called when adding this layer to a map
+     * @param {L.Map} map Leaflet map
+     */
+    addTo: function(map) {
+        map.addLayer(this);
+        return this;
+    },
 
-  onAdd: function (map) {
-    this._map = map;
-    if (!this._timeDimension && map.timeDimension) {
-      this._timeDimension = map.timeDimension;
-    }
-    this._timeDimension.on("timeloading", this._onNewTimeLoading, this);
-    this._timeDimension.on("timeload", this._update, this);
-    this._timeDimension.registerSyncedLayer(this);
-    this._update();
-  },
+    /**
+     * Method called when adding this layer to a map
+     * @param {L.Map} map Leaflet map
+     */
+    onAdd: function(map) {
+        this._map = map;
+        if (!this._timeDimension && map.timeDimension) {
+            this._timeDimension = map.timeDimension;
+        }
+        this._timeDimension.on("timeloading", this._onNewTimeLoading, this);
+        this._timeDimension.on("timeload", this._update, this);
+        this._timeDimension.registerSyncedLayer(this);
+        this._update();
+    },
 
-  onRemove: function (map) {
-    this._timeDimension.unregisterSyncedLayer(this);
-    this._timeDimension.off("timeloading", this._onNewTimeLoading, this);
-    this._timeDimension.off("timeload", this._update, this);
-    this.eachLayer(map.removeLayer, map);
-    this._map = null;
-  },
+    /**
+     * Method called when removing this layer to a map
+     * @param {L.Map} map Leaflet map
+     */
+    onRemove: function(map) {
+        this._timeDimension.unregisterSyncedLayer(this);
+        this._timeDimension.off("timeloading", this._onNewTimeLoading, this);
+        this._timeDimension.off("timeload", this._update, this);
+        this.eachLayer(map.removeLayer, map);
+        this._map = null;
+    },
 
-  eachLayer: function (method, context) {
-    method.call(context, this._baseLayer);
-    return this;
-  },
+    /**
+     * Method called when searching each layer
+     * @param {function} method 
+     * @param {object} context 
+     * @returns self return
+     */
+    eachLayer: function(method, context) {
+        method.call(context, this._baseLayer);
+        return this;
+    },
 
-  setZIndex: function (zIndex) {
-    this.options.zIndex = zIndex;
-    if (this._baseLayer.setZIndex) {
-      this._baseLayer.setZIndex(zIndex);
-    }
-    if (this._currentLayer && this._currentLayer.setZIndex) {
-      this._currentLayer.setZIndex(zIndex);
-    }
-    return this;
-  },
+    /**
+     * Sez a new Z index
+     * @param {number} zIndex new z index 
+     * @returns self return
+     */
+    setZIndex: function(zIndex) {
+        this.options.zIndex = zIndex;
+        if (this._baseLayer.setZIndex) {
+            this._baseLayer.setZIndex(zIndex);
+        }
+        if (this._currentLayer && this._currentLayer.setZIndex) {
+            this._currentLayer.setZIndex(zIndex);
+        }
+        return this;
+    },
 
-  setOpacity: function (opacity) {
-    this.options.opacity = opacity;
-    if (this._baseLayer.setOpacity) {
-      this._baseLayer.setOpacity(opacity);
-    }
-    if (this._currentLayer && this._currentLayer.setOpacity) {
-      this._currentLayer.setOpacity(opacity);
-    }
-    return this;
-  },
+    /**
+     * Set a new opacity
+     * @param {number} opacity new opacity
+     * @returns self return
+     */
+    setOpacity: function(opacity) {
+        this.options.opacity = opacity;
+        if (this._baseLayer.setOpacity) {
+            this._baseLayer.setOpacity(opacity);
+        }
+        if (this._currentLayer && this._currentLayer.setOpacity) {
+            this._currentLayer.setOpacity(opacity);
+        }
+        return this;
+    },
 
-  bringToBack: function () {
-    if (!this._currentLayer) {
-      return;
-    }
-    this._currentLayer.bringToBack();
-    return this;
-  },
+    /**
+     * Method called to bring the layer to the back
+     */
+    bringToBack: function() {
+        if (!this._currentLayer) {
+            return;
+        }
+        this._currentLayer.bringToBack();
+        return this;
+    },
 
-  bringToFront: function () {
-    if (!this._currentLayer) {
-      return;
-    }
-    this._currentLayer.bringToFront();
-    return this;
-  },
+    /**
+     * Method called to bring the layer to the front
+     */
+    bringToFront: function() {
+        if (!this._currentLayer) {
+            return;
+        }
+        this._currentLayer.bringToFront();
+        return this;
+    },
 
-  _onNewTimeLoading: function (ev) {
-    // to be implemented for each type of layer
-    this.fire("timeload", {
-      time: ev.time,
-    });
-    return;
-  },
+    /**
+     * Method called when loading a new time
+     * @param {object} ev 
+     * @returns Empty return
+     */
+    _onNewTimeLoading: function(ev) {
+        // to be implemented for each type of layer
+        this.fire("timeload", {
+            time: ev.time,
+        });
+        return;
+    },
 
-  isReady: function (time) {
-    // to be implemented for each type of layer
-    return true;
-  },
+    /**
+     * Method called to check if the layer is ready
+     * @param {number} time 
+     * @returns Always true
+     */
+    isReady: function(time) {
+        // to be implemented for each type of layer
+        return true;
+    },
 
-  _update: function () {
-    // to be implemented for each type of layer
-    return true;
-  },
+    /**
+     * Method called when updating the layer
+     * @returns Always true
+     */
+    _update: function() {
+        // to be implemented for each type of layer
+        return true;
+    },
 
-  getBaseLayer: function () {
-    return this._baseLayer;
-  },
+    /**
+     * Get base layer
+     * @returns Leaflet base layer
+     */
+    getBaseLayer: function() {
+        return this._baseLayer;
+    },
 
-  getBounds: function () {
-    var bounds = new LatLngBounds();
-    if (this._currentLayer) {
-      bounds.extend(
-        this._currentLayer.getBounds
-          ? this._currentLayer.getBounds()
-          : this._currentLayer.getLatLng()
-      );
-    }
-    return bounds;
-  },
+    /**
+     * Get layer bounds
+     * @returns layer bounds
+     */
+    getBounds: function() {
+        var bounds = new LatLngBounds();
+        if (this._currentLayer) {
+            bounds.extend(
+                this._currentLayer.getBounds ?
+                this._currentLayer.getBounds() :
+                this._currentLayer.getLatLng()
+            );
+        }
+        return bounds;
+    },
 });
--- src/leaflet.timedimension.util.js
+++ src/leaflet.timedimension.util.js
@@ -1,222 +1,285 @@
 import nezasa from "iso8601-js-period";
 
+/**
+ * Convert ISO-8601 period into a array of int
+ * @param {string} ISODuration ISO-8601 period
+ * @throws {error} nezasa dependency error
+ * @returns values as array of number (nezasa format)
+ */
 export function getTimeDuration(ISODuration) {
-  return nezasa.Period.parse(ISODuration, true);
+    return nezasa.Period.parse(ISODuration, true);
 }
 
+/**
+ * Add duration to date
+ * @param {Date} date initial date
+ * @param {number[]} duration nezasa array of datetime properties
+ * @param {boolean} utc universal time as flag
+ */
 export function addTimeDuration(date, duration, utc) {
-  if (typeof utc === "undefined") {
-    utc = true;
-  }
-  if (typeof duration == "string" || duration instanceof String) {
-    duration = getTimeDuration(duration);
-  }
-  var l = duration.length;
-  var get = utc ? "getUTC" : "get";
-  var set = utc ? "setUTC" : "set";
+    if (typeof utc === "undefined") {
+        utc = true;
+    }
+    if (typeof duration == "string" || duration instanceof String) {
+        duration = getTimeDuration(duration);
+    }
+    var l = duration.length;
+    var get = utc ? "getUTC" : "get";
+    var set = utc ? "setUTC" : "set";
 
-  if (l > 0 && duration[0] != 0) {
-    date[set + "FullYear"](date[get + "FullYear"]() + duration[0]);
-  }
-  if (l > 1 && duration[1] != 0) {
-    date[set + "Month"](date[get + "Month"]() + duration[1]);
-  }
-  if (l > 2 && duration[2] != 0) {
-    // weeks
-    date[set + "Date"](date[get + "Date"]() + duration[2] * 7);
-  }
-  if (l > 3 && duration[3] != 0) {
-    date[set + "Date"](date[get + "Date"]() + duration[3]);
-  }
-  if (l > 4 && duration[4] != 0) {
-    date[set + "Hours"](date[get + "Hours"]() + duration[4]);
-  }
-  if (l > 5 && duration[5] != 0) {
-    date[set + "Minutes"](date[get + "Minutes"]() + duration[5]);
-  }
-  if (l > 6 && duration[6] != 0) {
-    date[set + "Seconds"](date[get + "Seconds"]() + duration[6]);
-  }
+    if (l > 0 && duration[0] != 0) {
+        date[set + "FullYear"](date[get + "FullYear"]() + duration[0]);
+    }
+    if (l > 1 && duration[1] != 0) {
+        date[set + "Month"](date[get + "Month"]() + duration[1]);
+    }
+    if (l > 2 && duration[2] != 0) {
+        // weeks
+        date[set + "Date"](date[get + "Date"]() + duration[2] * 7);
+    }
+    if (l > 3 && duration[3] != 0) {
+        date[set + "Date"](date[get + "Date"]() + duration[3]);
+    }
+    if (l > 4 && duration[4] != 0) {
+        date[set + "Hours"](date[get + "Hours"]() + duration[4]);
+    }
+    if (l > 5 && duration[5] != 0) {
+        date[set + "Minutes"](date[get + "Minutes"]() + duration[5]);
+    }
+    if (l > 6 && duration[6] != 0) {
+        date[set + "Seconds"](date[get + "Seconds"]() + duration[6]);
+    }
 }
 
+/**
+ * Subtract duration to date
+ * @param {Date} date initial date
+ * @param {number[]} duration nezasa array of datetime properties
+ * @param {boolean} utc universal time as flag
+ */
 export function subtractTimeDuration(date, duration, utc) {
-  if (typeof duration == "string" || duration instanceof String) {
-    duration = getTimeDuration(duration);
-  }
-  var subDuration = [];
-  for (var i = 0, l = duration.length; i < l; i++) {
-    subDuration.push(-duration[i]);
-  }
-  addTimeDuration(date, subDuration, utc);
+    if (typeof duration == "string" || duration instanceof String) {
+        duration = getTimeDuration(duration);
+    }
+    var subDuration = [];
+    for (var i = 0, l = duration.length; i < l; i++) {
+        subDuration.push(-duration[i]);
+    }
+    addTimeDuration(date, subDuration, utc);
 }
 
+/**
+ * Get time range from time interval ISO string
+ * @param {string} timeRange time interval ISO string
+ * @param {string} overwritePeriod ISO-8601 period
+ * @returns range as array of time
+ */
 export function parseAndExplodeTimeRange(timeRange, overwritePeriod) {
-  var tr = timeRange.split("/");
-  var startTime = new Date(Date.parse(tr[0]));
-  var endTime = new Date(Date.parse(tr[1]));
-  var period = tr.length > 2 && tr[2].length ? tr[2] : "P1D";
-  if (overwritePeriod !== undefined && overwritePeriod !== null) {
-    period = overwritePeriod;
-  }
-  return explodeTimeRange(startTime, endTime, period);
+    var tr = timeRange.split("/");
+    var startTime = new Date(Date.parse(tr[0]));
+    var endTime = new Date(Date.parse(tr[1]));
+    var period = tr.length > 2 && tr[2].length ? tr[2] : "P1D";
+    if (overwritePeriod !== undefined && overwritePeriod !== null) {
+        period = overwritePeriod;
+    }
+    return explodeTimeRange(startTime, endTime, period);
 }
 
+/**
+ * Get array of time based on time interval ISO string
+ * @param {Date} startTime initial time
+ * @param {Date} endTime final time
+ * @param {string} ISODuration time interval as duration ISO string
+ * @param {string} validTimeRange base time interval ISO string
+ * @returns range as array of time
+ */
 export function explodeTimeRange(
-  startTime,
-  endTime,
-  ISODuration,
-  validTimeRange
+    startTime,
+    endTime,
+    ISODuration,
+    validTimeRange
 ) {
-  var duration = getTimeDuration(ISODuration);
-  var result = [];
-  var currentTime = new Date(startTime.getTime());
-  var minHour = null,
-    minMinutes = null,
-    maxHour = null,
-    maxMinutes = null;
-  if (validTimeRange !== undefined) {
-    var validTimeRangeArray = validTimeRange.split("/");
-    minHour = validTimeRangeArray[0].split(":")[0];
-    minMinutes = validTimeRangeArray[0].split(":")[1];
-    maxHour = validTimeRangeArray[1].split(":")[0];
-    maxMinutes = validTimeRangeArray[1].split(":")[1];
-  }
-  while (currentTime < endTime) {
-    if (
-      validTimeRange === undefined ||
-      (currentTime.getUTCHours() >= minHour &&
-        currentTime.getUTCHours() <= maxHour)
-    ) {
-      if (
-        (currentTime.getUTCHours() != minHour ||
-          currentTime.getUTCMinutes() >= minMinutes) &&
-        (currentTime.getUTCHours() != maxHour ||
-          currentTime.getUTCMinutes() <= maxMinutes)
-      ) {
-        result.push(currentTime.getTime());
-      }
+    var duration = getTimeDuration(ISODuration);
+    var result = [];
+    var currentTime = new Date(startTime.getTime());
+    var minHour = null,
+        minMinutes = null,
+        maxHour = null,
+        maxMinutes = null;
+    if (validTimeRange !== undefined) {
+        var validTimeRangeArray = validTimeRange.split("/");
+        minHour = validTimeRangeArray[0].split(":")[0];
+        minMinutes = validTimeRangeArray[0].split(":")[1];
+        maxHour = validTimeRangeArray[1].split(":")[0];
+        maxMinutes = validTimeRangeArray[1].split(":")[1];
     }
-    addTimeDuration(currentTime, duration);
-  }
-  if (currentTime >= endTime) {
-    result.push(endTime.getTime());
-  }
-  return result;
-}
-
-export function parseTimeInterval(timeInterval) {
-  var parts = timeInterval.split("/");
-  if (parts.length != 2) {
-    throw "Incorrect ISO8601 TimeInterval: " + timeInterval;
-  }
-  const startTime = Date.parse(parts[0]);
-  let startTimeDate;
-  let endTime;
-  let endTimeDate;
-  var duration = null;
-  if (isNaN(startTime)) {
-    // -> format duration/endTime
-    duration = getTimeDuration(parts[0]);
-    endTime = Date.parse(parts[1]);
-    startTimeDate = new Date(endTime);
-    subtractTimeDuration(startTime, duration, true);
-    endTimeDate = new Date(endTime);
-  } else {
-    endTime = Date.parse(parts[1]);
-    if (isNaN(endTime)) {
-      // -> format startTime/duration
-      duration = getTimeDuration(parts[1]);
-      endTimeDate = new Date(startTime);
-      addTimeDuration(endTime, duration, true);
-    } else {
-      // -> format startTime/endTime
-      endTimeDate = new Date(endTime);
-    }
-    startTimeDate = new Date(startTime);
-  }
-  return [startTimeDate, endTimeDate];
-}
-
-export function parseTimesExpression(times, overwritePeriod) {
-  var result = [];
-  if (!times) {
-    return result;
-  }
-  if (typeof times == "string" || times instanceof String) {
-    var timeRanges = times.split(",");
-    var timeRange;
-    var timeValue;
-    for (var i = 0, l = timeRanges.length; i < l; i++) {
-      timeRange = timeRanges[i];
-      if (timeRange.split("/").length == 3) {
-        result = result.concat(
-          parseAndExplodeTimeRange(timeRange, overwritePeriod)
-        );
-      } else {
-        timeValue = Date.parse(timeRange);
-        if (!isNaN(timeValue)) {
-          result.push(timeValue);
+    while (currentTime < endTime) {
+        if (
+            validTimeRange === undefined ||
+            (currentTime.getUTCHours() >= minHour &&
+                currentTime.getUTCHours() <= maxHour)
+        ) {
+            if (
+                (currentTime.getUTCHours() != minHour ||
+                    currentTime.getUTCMinutes() >= minMinutes) &&
+                (currentTime.getUTCHours() != maxHour ||
+                    currentTime.getUTCMinutes() <= maxMinutes)
+            ) {
+                result.push(currentTime.getTime());
+            }
         }
-      }
+        addTimeDuration(currentTime, duration);
     }
-  } else {
-    result = times;
-  }
-  return result.sort(function (a, b) {
-    return a - b;
-  });
+    if (currentTime >= endTime) {
+        result.push(endTime.getTime());
+    }
+    return result;
 }
 
+/**
+ * Get date ranges from a timeInterval
+ * @param {string} timeInterval time interval ISO string
+ * @throws {error} Time interval malformed
+ * @returns array with range of interval
+ */
+export function parseTimeInterval(timeInterval) {
+    var parts = timeInterval.split("/");
+    if (parts.length != 2) {
+        throw "Incorrect ISO8601 TimeInterval: " + timeInterval;
+    }
+    const startTime = Date.parse(parts[0]);
+    let startTimeDate;
+    let endTime;
+    let endTimeDate;
+    var duration = null;
+    if (isNaN(startTime)) {
+        // -> format duration/endTime
+        duration = getTimeDuration(parts[0]);
+        endTime = Date.parse(parts[1]);
+        startTimeDate = new Date(endTime);
+        subtractTimeDuration(startTime, duration, true);
+        endTimeDate = new Date(endTime);
+    } else {
+        endTime = Date.parse(parts[1]);
+        if (isNaN(endTime)) {
+            // -> format startTime/duration
+            duration = getTimeDuration(parts[1]);
+            endTimeDate = new Date(startTime);
+            addTimeDuration(endTime, duration, true);
+        } else {
+            // -> format startTime/endTime
+            endTimeDate = new Date(endTime);
+        }
+        startTimeDate = new Date(startTime);
+    }
+    return [startTimeDate, endTimeDate];
+}
+
+/**
+ * Get time range from time interval ISO string
+ * @param {string} time time interval ISO string
+ * @param {string} overwritePeriod ISO-8601 period
+ * @returns range as array of time
+ */
+export function parseTimesExpression(times, overwritePeriod) {
+    var result = [];
+    if (!times) {
+        return result;
+    }
+    if (typeof times == "string" || times instanceof String) {
+        var timeRanges = times.split(",");
+        var timeRange;
+        var timeValue;
+        for (var i = 0, l = timeRanges.length; i < l; i++) {
+            timeRange = timeRanges[i];
+            if (timeRange.split("/").length == 3) {
+                result = result.concat(
+                    parseAndExplodeTimeRange(timeRange, overwritePeriod)
+                );
+            } else {
+                timeValue = Date.parse(timeRange);
+                if (!isNaN(timeValue)) {
+                    result.push(timeValue);
+                }
+            }
+        }
+    } else {
+        result = times;
+    }
+    return result.sort(function(a, b) {
+        return a - b;
+    });
+}
+
+/**
+ * Group array by intersection
+ * @param {number[]} arrayA first array
+ * @param {number[]} arrayB second array
+ * @returns new array with intersected values
+ */
 export function intersect_arrays(arrayA, arrayB) {
-  var a = arrayA.slice(0);
-  var b = arrayB.slice(0);
-  var result = [];
-  while (a.length > 0 && b.length > 0) {
-    if (a[0] < b[0]) {
-      a.shift();
-    } else if (a[0] > b[0]) {
-      b.shift();
-    } /* they're equal */ else {
-      result.push(a.shift());
-      b.shift();
+    var a = arrayA.slice(0);
+    var b = arrayB.slice(0);
+    var result = [];
+    while (a.length > 0 && b.length > 0) {
+        if (a[0] < b[0]) {
+            a.shift();
+        } else if (a[0] > b[0]) {
+            b.shift();
+        } /* they're equal */
+        else {
+            result.push(a.shift());
+            b.shift();
+        }
     }
-  }
-  return result;
+    return result;
 }
 
+/**
+ * Group array by union
+ * @param {number[]} arrayA first array
+ * @param {number[]} arrayB second array
+ * @returns new array with united values
+ */
 export function union_arrays(arrayA, arrayB) {
-  var a = arrayA.slice(0);
-  var b = arrayB.slice(0);
-  var result = [];
-  while (a.length > 0 && b.length > 0) {
-    if (a[0] < b[0]) {
-      result.push(a.shift());
-    } else if (a[0] > b[0]) {
-      result.push(b.shift());
-    } /* they're equal */ else {
-      result.push(a.shift());
-      b.shift();
+    var a = arrayA.slice(0);
+    var b = arrayB.slice(0);
+    var result = [];
+    while (a.length > 0 && b.length > 0) {
+        if (a[0] < b[0]) {
+            result.push(a.shift());
+        } else if (a[0] > b[0]) {
+            result.push(b.shift());
+        } /* they're equal */
+        else {
+            result.push(a.shift());
+            b.shift();
+        }
     }
-  }
-  if (a.length > 0) {
-    result = result.concat(a);
-  } else if (b.length > 0) {
-    result = result.concat(b);
-  }
-  return result;
+    if (a.length > 0) {
+        result = result.concat(a);
+    } else if (b.length > 0) {
+        result = result.concat(b);
+    }
+    return result;
 }
 
+/**
+ * Sort array and remove duplicated values
+ * @param {number[]} arr array to be sorted
+ * @returns array sorted and without duplicated values
+ */
 export function sort_and_deduplicate(arr) {
-  arr = arr.slice(0).sort(function (a, b) {
-    return a - b;
-  });
-  var result = [];
-  var last = null;
-  for (var i = 0, l = arr.length; i < l; i++) {
-    if (arr[i] !== last) {
-      result.push(arr[i]);
-      last = arr[i];
+    arr = arr.slice(0).sort(function(a, b) {
+        return a - b;
+    });
+    var result = [];
+    var last = null;
+    for (var i = 0, l = arr.length; i < l; i++) {
+        if (arr[i] !== last) {
+            result.push(arr[i]);
+            last = arr[i];
+        }
     }
-  }
-  return result;
+    return result;
 }
